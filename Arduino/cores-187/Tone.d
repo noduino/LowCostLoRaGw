64498da9d625114a5be0885d21e187e805a55215
Jack Tan <jiankemeng@gmail.com>
2019-05-20 17:15:54 +0800

    add the arduino cores based on 1.8.7

---
+++ Arduino/cores-187/Tone.d
@@ -0,0 +1,5 @@
+../cores-187/Tone.o: ../cores-187/Tone.cpp ../cores-187/Arduino.h \
+ ../cores-187/binary.h ../cores-187/WCharacter.h ../cores-187/WString.h \
+ ../cores-187/HardwareSerial.h ../cores-187/Stream.h ../cores-187/Print.h \
+ ../cores-187/Printable.h ../cores-187/USBAPI.h \
+ ../cores-187/pins_arduino.h
